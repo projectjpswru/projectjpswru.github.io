5ede7a7e78d3ae8fa7bbeae6bded8eb7bf831a1e
TheFunnyDay <39840774+TheFunnyDay@users.noreply.github.com>
2019-08-19 03:03:22 +0500

    9.3.4

--- css/menu.js
+++ css/menu.js
@@ -1,6 +1,6 @@
 window.onscroll = function showHeader() {
     var header = document.querySelector('.navbar');
-    if(window.pageYOffset > 200){
+    if(window.pageYOffset > 75){
         header.classList.add('navbar_fixed');
     } else{
         header.classList.remove('navbar_fixed');

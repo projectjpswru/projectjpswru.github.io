4333b2e7decce698ef9f256783ca76c79a625264
TheFunnyDay <39840774+TheFunnyDay@users.noreply.github.com>
2019-08-18 23:56:01 +0500

    9.2.3
    +new character
    menu update

--- css/menu.js
+++ css/menu.js
@@ -1,7 +1,8 @@
-	$('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
-	
-	$("#menu-icon").on("click", function(){
-    $("nav").slideToggle();
-    $(this).toggleClass("active");
-});
-
+window.onscroll = function showHeader() {
+    var header = document.querySelector('.navbar');
+    if(window.pageYOffset > 200){
+        header.classList.add('navbar_fixed');
+    } else{
+        header.classList.remove('navbar_fixed');
+    }
+}

257000c461eeb4d94d6d60ccd618e9f92ab0b3ee
TheFunnyDay <39840774+TheFunnyDay@users.noreply.github.com>
2019-08-29 05:19:08 +0500

    11.4.6

--- css/menu.js
+++ css/menu.js
@@ -1,8 +1,14 @@
-window.onscroll = function showHeader() {
+window.onscroll = function showHeader() 
+
+{
     var header = document.querySelector('.navbar');
     if(window.pageYOffset > 150){
         header.classList.add('navbar_fixed');
     } else{
         header.classList.remove('navbar_fixed');
     }
+
 }
+
+
+
